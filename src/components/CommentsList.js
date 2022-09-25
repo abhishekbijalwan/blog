57688c03d97ca6de3252a8e313901dd36934ec87
Abhishek Bijalwan <abhishek.bijalwan092@gmail.com>
2022-09-25 22:17:01 +0530

    Add key to comments list

--- src/components/CommentsList.js
+++ src/components/CommentsList.js
@@ -8,7 +8,7 @@ export default function CommentsList() {
     <div className="flex items-center justify-center">
       <div className="block">
         {comments?.map(({ name, body }) => (
-          <div className="my-4 flex items-center justify-center overflow-hidden">
+          <div key={name} className="my-4 flex items-center justify-center overflow-hidden">
             <div className="h-24 w-1/2 rounded-xl bg-gray-100 p-3">
               <div className="font-medium ">
                 <small>{name}</small>
